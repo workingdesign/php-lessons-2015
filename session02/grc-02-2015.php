032847249a31022ed41b673e206a3005dfce441d
Giorgio Riccardi <griccardi@my.bcit.ca>
2015-10-08 08:20:51 -0700

    Revert "TESTING HIGHLIGHT.JS"
    This reverts commit 65faf42d16498395afe752eb5f9f0761400060b3.

--- session02/grc-02-2015.php
+++ session02/grc-02-2015.php
@@ -11,17 +11,15 @@ const SCRIPTS_PATH = "./js/";
 
 $lessonNumber = 2;
 $myCss = "style.css";
-// $myCssHighlighter = "prism.css";
-// $myJsHighlighter = "prism.js";
+$myCssHighlighter = "prism.css";
+$myJsHighlighter = "prism.js";
 
 ?>
 
 	<title>PHP lesson <?php echo $lessonNumber; ?></title>
 
 	<link rel="stylesheet" href="<?php echo STYLESHEET_PATH . $myCss; ?>" type="text/css">
-	<link rel="stylesheet" href="<?php //echo STYLESHEET_PATH . $myCssHighlighter; ?>" type="text/css">
-	<!-- https://highlightjs.org -->
-	<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/highlight.js/8.8.0/styles/default.min.css">
+	<link rel="stylesheet" href="<?php echo STYLESHEET_PATH . $myCssHighlighter; ?>" type="text/css">
 </head>
 <body>
 	<header>
@@ -43,30 +41,11 @@ $myCss = "style.css";
 
 	<footer>
 		<h4>
-			TESTING HIGHLIGHT.JS
+			<!-- Exercise completed. -->
 		</h4>
 	</footer>
 
-<pre><code class="php">
-	the outcome is pretty much the same as PRISM.JS
-
-	// pre-existing HTML is stripped off in highlight.js as well
-
-	echo '&lt;p>' . 'My PHP name is' . ' ' . $myName . '&lt;/p>';
-
-	echo '&lt;p>and my height [' . $myHeightCm . 'cm] in feet is:&lt;/p>';
-
-	echo $myHeightCm / 30.48 . '\'' . '&lt;br>';
-
-	echo '&lt;pre>&lt;code>{FT = CM/30.48 <---> CM = 30.48*FT}&lt;/code>&lt;/pre>';
-
-	echo '&lt;p>or using "&lt;em>round&lt;/em>" [' . $roundLink . '] is: &lt;br>&lt;br> '	. round ( $myHeightCm / 30.48 ) . '\'&lt;/p>';
-</code></pre>
-
-<script src="<?php //echo SCRIPTS_PATH . $myJsHighlighter; ?>"></script>
-<!-- https://highlightjs.org -->
-<script src="//cdnjs.cloudflare.com/ajax/libs/highlight.js/8.8.0/highlight.min.js"></script>
-<script>hljs.initHighlightingOnLoad();</script>
+<script src="<?php echo SCRIPTS_PATH . $myJsHighlighter; ?>"></script>
 
 </body>
 </html>
